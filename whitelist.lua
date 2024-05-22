64db84c6e9f721c331831671354c29d07b5efef8
CloudDev <90874854+OperationOfTheCloud@users.noreply.github.com>
2024-05-22 14:57:37 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,4 @@
-if game.Players.LocalPlayer.Name == "VoidAlt9999" or game.Players.LocalPlayer.Name == "GyattRizzFanumTaxers" then
+if game.Players.LocalPlayer.Name == "VoidAlt9999" or game.Players.LocalPlayer.Name == "GyattRizzFanumTaxers" or game.Players.LocalPlayer.Name == "0_10068" then
 else
   game.Players.LocalPlayer:Kick()
   while true do
